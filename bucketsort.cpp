7e249d69a4022ab584f43caec0fea6c75cfb7c66
spitfire4040 <jthom@cse.unr.edu>
2016-11-17 16:18:17 -0800

    made change

--- bucketsort.cpp
+++ bucketsort.cpp
@@ -3,6 +3,20 @@
 using namespace std;
 
 int main(){
+
+	// read input list of values to be sorted
+
+
+	// find max value in list
+
+
+	// create array of unsorted values and read in values
+
+
+	// create result array (index)
+
+
+	// random change
 	
 
 	return 0
